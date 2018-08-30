0ccb2d732cc7f41d4ceca3ccd906c219b31789bd
ThomasLee94 <thomaslee9474@gmail.com>
2018-08-29 17:23:18 -0700

    Initial commit

---
+++ MadLibsMVP.py
@@ -0,0 +1,24 @@
+# Name: Thomas J. Lee
+# Project: Mad Libs MVP
+
+# Requirements:
+# - Display story with blanks for input words with correct type (nouns, verbs, ... etc.)
+# - Ask user to input words to fill in the blanks.
+# - Output the story with the blanks filled in.
+# - A test suite to test user input.
+
+# Stretch Requirements:
+# Randomize the words of the same type (ie shuffle the 5 nouns)).
+# Use a dictionary to generate the words.
+# Use a differnet data structure to store words.
+# Build with TDD
+# Use the system module (for accessing command-line arguments)
+
+# Code Requirements:
+# Variable assignment
+# Function definitions
+# Core data types: strings, integers, floats
+# Collection types: lists, tuples, dictionary
+
+
+sentence = "There is a house that needs to be fixed"
